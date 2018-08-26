119b90453024f29495a577b2e3a5338d170f7023
olivier <42643094+oliviez84@users.noreply.github.com>
2018-08-26 19:15:05 +0200

    new

---
+++ js/main.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+
+
+    $('.owl-carousel').owlCarousel({
+    
+        items:1,
+        animateOut: 'slideOutDown',
+        animateIn: 'flipInX',
+        // margin:30,
+        // stagePadding:30,
+        autoplayHoverPause: true,
+        autoplay: true,
+        autoplayTimeout:5000,
+        loop: true,
+        pagination: true,
+        dots: true,
+        smartSpeed:450,
+        nav: false,
+       points: false,
+       dotsEach: true
+    });
+
+    $(window).scroll(function(e){
+        var scrolltop = $(this).scrollTop()
+
+       if(scrolltop > 99 ){
+           $('.menu').addClass('menutop')
+           $('.menu2').css('display','flex')
+       }else {
+        $('.menu').removeClass('menutop')
+        $('.menu2').css('display','none')
+       }
+    })
+
+    $(window).scroll(function(e){
+        var scrolltop = $(this).scrollTop()
+      
+       if(scrolltop > 300 && scrolltop < 1000){
+        $('.compt .colon').addClass('animated slideInRight delay-2s')
+    }else {
+        $('.compt .colon').removeClass('animated slideInRight delay-2s')
+    }
+     
+    })
+
+
+    $('.menu a').click(function(e){
+        e.preventDefault() 
+        var $link=$(this).attr('href')
+        pos = $($link).offset()
+        var scol= pos.top - 80
+    
+         $('body,html').animate({scrollTop:scol},600)
+       
+})
+$('.menu-icone').click(function(e){
+    e.preventDefault();
+    $this = $(this);
+    if($this.hasClass('is-open')){
+    $this.addClass('is-closed').removeClass('is-open');
+   }else {
+       $this.removeClass('is-closed').addClass('is-open');
+   }
+
+    $('#sites span').slideToggle("slow");
+
+})
+
+})
