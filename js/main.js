93186f0467652d026e80affa46d4f8c00f6a7969
olivier <42643094+oliviez84@users.noreply.github.com>
2018-08-28 15:02:42 +0200

    recalcule

--- js/main.js
+++ js/main.js
@@ -74,8 +74,8 @@ $(document).ready(function(){
 
     $(window).scroll(function(e){
         var scrolltop = $(this).scrollTop()
-      
-       if(scrolltop > 300 && scrolltop < 1000){
+     
+       if(scrolltop > 1600 && scrolltop < 2000){
         $('.compt .colon').addClass('animated slideInRight delay-2s')
     }else {
         $('.compt .colon').removeClass('animated slideInRight delay-2s')
