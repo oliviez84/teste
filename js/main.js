b89a1bdf7a3993d93c5343a2f037107528f6fd2d
olivier <42643094+oliviez84@users.noreply.github.com>
2018-11-28 17:47:31 +0100

    :maj .8

--- js/main.js
+++ js/main.js
@@ -1,62 +1,6 @@
 $(document).ready(function(){
 
    
-      
-
-    $('#rea2 .owl-carousel').owlCarousel({
-    
-        items:2,
-        animateOut: 'slideOutDown',
-        animateIn: 'flipInX',
-        // margin:30,
-        // stagePadding:30,
-        autoplayHoverPause: true,
-        autoplay: true,
-        autoplayTimeout:5000,
-        loop: true,
-        pagination: true,
-        dots: true,
-        smartSpeed:450,
-        nav: false,
-       points: false,
-       dotsEach: true
-    });
-    $('#rea .owl-carousel').owlCarousel({
-    
-        items:2,
-        animateOut: 'slideOutDown',
-        animateIn: 'flipInX',
-        // margin:30,
-        // stagePadding:30,
-        autoplayHoverPause: true,
-        autoplay: true,
-        autoplayTimeout:5000,
-        loop: true,
-        pagination: true,
-        dots: true,
-        smartSpeed:450,
-        nav: false,
-       points: false,
-       dotsEach: true
-    });
-    $('.owl-carousel').owlCarousel({
-    
-        items:1,
-        animateOut: 'slideOutDown',
-        animateIn: 'flipInX',
-        // margin:30,
-        // stagePadding:30,
-        autoplayHoverPause: true,
-        autoplay: true,
-        autoplayTimeout:5000,
-        loop: true,
-        pagination: true,
-        dots: true,
-        smartSpeed:450,
-        nav: false,
-       points: false,
-       dotsEach: true
-    });
 
    
    
@@ -65,45 +9,39 @@ $(document).ready(function(){
 
        if(scrolltop > 99 ){
            $('.menu').addClass('menutop')
-           $('.menu2').css('display','flex')
+          
        }else {
         $('.menu').removeClass('menutop')
-        $('.menu2').css('display','none')
+   
        }
-    })
+    });
 
-    $(window).scroll(function(e){
-        var scrolltop = $(this).scrollTop()
-     
-       if(scrolltop > 1200 && scrolltop < 1600){
-        $('.compt .colon').addClass('animated bounceInUp delay-2s')
-    }else {
-        $('.compt .colon').removeClass('animated bounceInUp delay-2s')
-    }
-     
-    })
+ 
 
 
     $('.menu a').click(function(e){
-        e.preventDefault() 
+        // e.preventDefault() 
         var $link=$(this).attr('href')
         pos = $($link).offset()
         var scol= pos.top -80
     
-         $('body,html').animate({scrollTop:scol},600)
+         $('body,html').animate({scrollTop:scol},600);
+         console.log('boom')
        
 })
-$('.menu-icone').click(function(e){
-    e.preventDefault();
-    $this = $(this);
-    if($this.hasClass('is-open')){
-    $this.addClass('is-closed').removeClass('is-open');
-   }else {
-       $this.removeClass('is-closed').addClass('is-open');
-   }
 
-    $('#sites span').slideToggle("slow");
 
+$('#burger2 #menu-icone').click(function(e){
+ e.preventDefault();
+
+ $this = $(this);
+ if($this.hasClass('is-open')){
+     $this.addClass('is-closed').removeClass('is-open');  
+ }else {
+     $this.removeClass('is-closed').addClass('is-open');
+ }
+   $('.menu-2 ul').toggleClass('open');
+ 
 })
 
 })
