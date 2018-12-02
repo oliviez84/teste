aeefd4755258dc7bf9d6815fb468325ff4ab3a7c
olivier <42643094+oliviez84@users.noreply.github.com>
2018-12-02 18:39:34 +0100

    final final

--- js/main.js
+++ js/main.js
@@ -15,6 +15,39 @@ $(document).ready(function(){
    
        }
     });
+    $(window).scroll(function(e){
+        var scrolltop = $(this).scrollTop()
+        
+       if(scrolltop >3006 ){
+           $('.simon').addClass('rotate')
+          
+       }else {
+        $('.simon').removeClass('rotate')
+   
+       }
+    });
+    $(window).scroll(function(e){
+        var scrolltop = $(this).scrollTop()
+        
+       if(scrolltop >850){
+           $('.simon1').addClass('rotate2')
+          
+       }else {
+        $('.simon1').removeClass('rotate2')
+   
+       }
+    });
+    $(window).scroll(function(e){
+        var scrolltop = $(this).scrollTop()
+        
+       if(scrolltop >1900){
+           $('.simon2').addClass('rotate3')
+          
+       }else {
+        $('.simon2').removeClass('rotate3')
+   
+       }
+    });
 
  
 
@@ -26,7 +59,7 @@ $(document).ready(function(){
         var scol= pos.top -20
     
          $('body,html').animate({scrollTop:scol},600);
-         console.log('boom')
+       
        
 })
 
